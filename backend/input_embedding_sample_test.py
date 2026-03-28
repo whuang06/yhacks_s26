ae3a5cc6dbfade5b021a2ba6475331a8920c98c6
whuang06 <william1993huang@gmail.com>
2026-03-28 13:04:32 -0400

    test branch

--- backend/input_embedding_sample_test.py
+++ backend/input_embedding_sample_test.py
@@ -4,3 +4,5 @@ file_path = "/Users/william/yhacks_s26/golem_sample_space_exact_names/5xm98t580u
 description = "This is an image of an artist"
 embedding = get_multimodal_embedding(file_path, description)
 print(embedding)
+
+print("test")
